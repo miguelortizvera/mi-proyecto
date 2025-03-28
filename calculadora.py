116b72b31f63832b4257106643f31471afbc180b
miguelortizvera <changeme@example.com>
2025-03-28 15:50:00 +0000

    se agrega resta

--- calculadora.py
+++ calculadora.py
@@ -4,3 +4,4 @@ num1 = int(input("Ingrese el primer numero: "))
 num2 = int(input("Ingrese el segundo numero: "))
 
 print(f"La suma de los 2 valores es {num1 + num2}")
+print(f"La resta de los 2 valores es {num1 - num2}")
